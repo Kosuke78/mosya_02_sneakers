875e8d00a8cecb06dcf7aecc2be2724ce8540bec
Kosuke <bkpc9070@yahoo.ne.jp>
2021-09-19 16:04:47 +0900

    second-commit

--- Desktop/mosya_Sneaker/mosya_02_Sneaker/js/style.js
+++ Desktop/mosya_Sneaker/mosya_02_Sneaker/js/style.js
@@ -4,14 +4,4 @@ jQuery(function($){
     movie.controls = false;
     });
 
-// 画面幅に画像を表示（縦）
-$(document).ready(function () {
-    hsize = $(window).height() - document.getElementById("header_inner").clientHeight;
-    alert(hsize);
-    $('div[class*="fv"]').css("height", hsize + "px");
-  });
-  $(window).resize(function () {
-    hsize = $(window).height() - document.getElementById("header_inner").clientHeight;
-    $('div[class*="fv"]').css("height", hsize + "px");
-    8;
-  }); 
+
